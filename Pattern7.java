129427cff1bab1e594e85c210ffe2402270cd8c2
pragathi001 <147241804+pragathi001@users.noreply.github.com>
2023-11-03 16:00:38 +0530

    Add files via upload

---
+++ Pattern7.java
@@ -0,0 +1,42 @@
+package Pattern.pyramids;
+
+import java.util.Scanner;
+
+public class Pattern7 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		int no = sc.nextInt();
+		
+		System.out.println("Enter the symbol : ");
+		char ch = sc.next().charAt(0);
+		
+		for(int i=1;i<no;i++) {
+			for(int j=1;j<=i;j++) {
+				if(j==1 || i ==j) {
+					System.out.print(ch);
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+		for(int i=no;i>=1;i--) {
+			for(int j=1;j<=i;j++) {
+				if(j==1 || i ==j ) {
+					System.out.print(ch);
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+		
+		
+	  }
+	}
+
+
